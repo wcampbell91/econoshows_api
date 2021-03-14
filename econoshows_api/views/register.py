bee0feeb7e523f43de680327899bc3f9e62244e3
Billy Campbell <wcampbell91@gmail.com>
2021-03-14 10:59:18 -0500

    fixed login_user method to allow a successful login from the client

--- econoshows_api/views/register.py
+++ econoshows_api/views/register.py
@@ -21,7 +21,12 @@ def login_user(request):
 
         if authenticated_user is not None:
             token = Token.objects.get(user=authenticated_user)
-            data = json.dumps({"valid": True, "token": token.key, "id": authenticated_user.id})
+            try:
+                band = Band.objects.get(user=authenticated_user)
+                data = json.dumps({"valid": True, "token": token.key, "id": authenticated_user.id, "band_id": band.id})
+            except:
+                venue = Venue.objects.get(user=authenticated_user)
+                data = json.dumps({"valid": True, "token": token.key, "id": authenticated_user.id, "venue_id": venue.id})
             return HttpResponse(data, content_type='application/json')
 
         else:
