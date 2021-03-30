7e5edf20bec27b93468bef5ed3008a79f496cdc8
Billy Campbell <wcampbell91@gmail.com>
2021-03-30 10:28:26 -0500

    procfile?

--- econoshows/settings.py
+++ econoshows/settings.py
@@ -142,14 +142,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT= 'media'
 MEDIA_URL='/media/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 django_heroku.settings(locals())
