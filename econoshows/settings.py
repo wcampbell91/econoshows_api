abc08e5e165aef4f3503cbd1c4da5d09979c1613
Billy Campbell <wcampbell91@gmail.com>
2021-03-30 09:56:51 -0500

    deploy `

--- econoshows/settings.py
+++ econoshows/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,8 +141,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = Path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT= 'media'
 MEDIA_URL='/media/'
+
+STATICFILES_DIRS = (
+    Path.join(BASE_DIR, 'static'),
+)
+
+django_heroku.settings(locals())
